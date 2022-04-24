6fe120a105522f6a26154ef57ae892782e40779f
Chris Risden <christopherrisden@gmail.com>
2022-04-24 16:50:59 -0700

    fixed links not going to a new page

--- src/Project.js
+++ src/Project.js
@@ -3,12 +3,12 @@ import './project.css';
 function Project(props) {
   return (
     <div className="project">
-      <a href={props.link}>
+      <a target="_blank" rel="noreferrer" href={props.link}>
         <h4>
           {props.title}
         </h4>
       </a>
-      <a href={props.repo}>
+      <a target="_blank" rel="noreferrer" href={props.repo}>
         Code Repo
       </a>
       <div>
